aef6cf3373d29a24059a92b747ae19ccadfd68fb
dbortoluzzi <danieleb88@gmail.com>
2020-10-25 15:43:57 +0100

    add logout button

--- frontend/frontend-app/src/app/core/footer/footer.component.ts
+++ frontend/frontend-app/src/app/core/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from "../../authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-footer',
@@ -9,7 +11,7 @@ export class FooterComponent implements OnInit {
 
   currentYear: number;
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
 
@@ -17,4 +19,12 @@ export class FooterComponent implements OnInit {
 
   }
 
+  logout(): void {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  isLogged(): boolean {
+    return this.authenticationService.currentUserValue != null;
+  }
 }
